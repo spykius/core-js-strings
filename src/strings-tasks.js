1b96c340902d7a9f3e9c21a18482cf2182479ad9
Nikita Korp <betaquarius203@gmail.com>
2024-11-04 13:08:47 +0300

    feat: implement reverseWords function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -365,8 +365,11 @@ function findLongestWord(sentence) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
-  throw new Error('Not implemented');
+function reverseWords(str) {
+  return str
+    .split(' ')
+    .map((value) => value.split('').reverse().join(''))
+    .join(' ');
 }
 
 /**
