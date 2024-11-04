3e782a9c1f61a430795ca29d4fa39e65d02d5e56
Nikita Korp <betaquarius203@gmail.com>
2024-11-04 13:06:49 +0300

    feat: implement findLongestWord function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -349,8 +349,10 @@ function isPalindrome(str) {
  *   findLongestWord('A long and winding road') => 'winding'
  *   findLongestWord('No words here') => 'words'
  */
-function findLongestWord(/* sentence */) {
-  throw new Error('Not implemented');
+function findLongestWord(sentence) {
+  return sentence
+    .split(' ')
+    .reduce((longest, word) => (word.length > longest.length ? word : longest));
 }
 
 /**
