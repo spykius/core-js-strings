6ecd872aaf7abfe1502e09f7c8ab757017d76791
Nikita Korp <betaquarius203@gmail.com>
2024-11-04 10:52:26 +0300

    feat: implement removeFirstOccurrences function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -146,8 +146,11 @@ function repeatString(str, times) {
  *   removeFirstOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeFirstOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeFirstOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeFirstOccurrences(str, value) {
+  const index = str.indexOf(value);
+  return index !== -1
+    ? str.slice(0, index) + str.slice(index + value.length)
+    : str;
 }
 
 /**
