cb92ada8536a275092bf66f85b8338f5cb5856a6
Nikita Korp <betaquarius203@gmail.com>
2024-11-04 18:12:24 +0300

    feat: implement getCardId function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -512,8 +512,28 @@ function encodeToRot13(str) {
  *   'Q♠' => 50
  *   'K♠' => 51
  */
-function getCardId(/* value */) {
-  throw new Error('Not implemented');
+function getCardId(value) {
+  const ranks = [
+    'A',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    'J',
+    'Q',
+    'K',
+  ];
+  const suits = ['♣', '♦', '♥', '♠'];
+
+  const rank = ranks.indexOf(value.slice(0, value.length - 1));
+  const suit = suits.indexOf(value.at(-1));
+
+  return ranks.length * suit + rank;
 }
 
 module.exports = {
