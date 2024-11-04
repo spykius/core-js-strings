de912b0f254317de24752a3efb5879a89e133a97
Nikita Korp <betaquarius203@gmail.com>
2024-11-04 10:16:05 +0300

    feat: implement repeatString function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -130,8 +130,8 @@ function removeTrailingWhitespaces(value) {
  *   repeatString('', 3) => ''
  *   repeatString('abc', -2) => ''
  */
-function repeatString(/* str, times */) {
-  throw new Error('Not implemented');
+function repeatString(str, times) {
+  return times >= 0 ? str.repeat(times) : '';
 }
 
 /**
